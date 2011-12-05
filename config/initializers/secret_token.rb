2d18cf3b9426678e27e12d0a32d579b0ed7b33e3
David Hendee <david@carbonfive.com>
2011-12-04 19:32:20 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gdyup::Application.config.secret_token = '58ea3d731f9b16610949aae87b11e7b6221f678f9f928fbce67dbac19cfd92edc9f55b7dc4a59644cb077d5e1b1826fc0c137785833b36971df5ebbd286e2a7a'
